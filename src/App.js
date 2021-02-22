4ddaa145bce69aebd5c1dc0d69bfeb9febcd63d9
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-02-22 15:31:25 -0500

    Created the Logo component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
 
 const App = () => {
   return (
     <div className="App">
       <Navigation />
+      <Logo />
     </div>
   );
 };
