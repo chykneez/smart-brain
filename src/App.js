01144842c3ce402ae531fb3acad2eb34f987b6d7
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-02-22 15:55:16 -0500

    Implemented particles library for styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
 
 import Navigation from './Components/Navigation/Navigation';
@@ -6,9 +7,61 @@ import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Entry from './Components/Entry/Entry';
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    shape: {
+      type: 'circle',
+      stroke: {
+        width: 0,
+        color: '#000000',
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+    },
+    color: {
+      value: '#ffffff',
+    },
+    opacity: {
+      value: 1,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 1,
+        sync: true,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: 'window',
+    events: {
+      onhover: {
+        enable: true,
+        mode: 'repulse',
+      },
+      resize: true,
+    },
+    modes: {
+      repulse: {
+        distance: 100,
+        duration: 0.4,
+      },
+    },
+  },
+};
+
 const App = () => {
   return (
     <div className="App">
+      <Particles id="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
       <Entry />
