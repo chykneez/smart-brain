1e2f179f6a1952fc2fd8679de985f346c595424f
Patrick Nguyen <iamtiredofyou2@gmail.com>
2021-02-22 15:50:01 -0500

    Created the Entry component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Entry from './Components/Entry/Entry';
 
 const App = () => {
   return (
     <div className="App">
       <Navigation />
       <Logo />
+      <Entry />
       <ImageLinkForm />
     </div>
   );
